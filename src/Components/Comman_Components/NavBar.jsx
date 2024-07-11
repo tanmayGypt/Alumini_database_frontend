1916ce644c27cdfa4d743ece49eac775d2fc9b8b
Aditya Singh <aditya2007singh2004@gmail.com>
2024-07-11 18:13:00 +0530

    css of login updated

--- src/Components/Comman_Components/NavBar.jsx
+++ src/Components/Comman_Components/NavBar.jsx
@@ -307,7 +307,7 @@ const NavBar = () => {
             <li>
               <button
                 onClick={handleLogout}
-                className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none"
+                className="nav-link-button"
               >
                 Logout
               </button>
@@ -316,7 +316,7 @@ const NavBar = () => {
             <li>
               <button
                 onClick={handleLogin}
-                className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none"
+                className="nav-link-button"
               >
                 Login
               </button>
