ee2c363613253d40e1a77c41255ffc669aa1019e
Sumit Joshi <sumit.joshi0616@gmail.com>
2024-07-19 20:51:35 +0530

    done the work

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,7 @@ function App() {
       <LoadingBar color="#9333ea" height={3} progress={progress} />
         
       <Footer />
+      <MainPage/>
     </>
   );
 }
--- src/Page/Explore/MainPage.jsx
+++ src/Page/Explore/MainPage.jsx
@@ -4,12 +4,12 @@ import Card from './Card';
 
 const MainPage = () => {
   return (
-    <div className="container">
-      <header className="header">
-        <h1 className="title">
-          EXPLORE OUR '<span className="highlight">ABHIGYAN</span>'ALUMNI NETWORK
+    <div className="container-main">
+      <header className="header11">
+        <h1 className="title-main">
+          EXPLORE OUR '<span className="highlight11">ABHIGYAN</span>'ALUMNI NETWORK
         </h1>
-        <p className="subtitle">
+        <p className="subtitle11">
           Effortlessly navigate through all sections to connect, engage, and stay updated.
         </p>
       </header>
@@ -17,32 +17,32 @@ const MainPage = () => {
         <Card 
           title="NEWS NETWORK" 
           description="The Alumni News Network provides the latest updates and highlights from our alumni community, showcasing their achievements and contributions." 
-          image="/news.jpg" 
+          image="/explore_alumni.jpg" 
         />
         <Card 
           title="ALUMNI NETWORKING & MENTORSHIP" 
           description="Join events, reunions, and mentorship programs to expand your professional network and connect with fellow alumni." 
-          image="/eventimage.jpg" 
+          image="/explore_event.jpg" 
         />
         <Card 
           title="ACHIEVEMENTS" 
           description="Celebrate the success of our alumni by browsing through their notable achievements and milestones in various fields." 
-          image="/alumni.jpg" 
+          image="/explore_news.jpg" 
         />
         <Card 
           title="ALUMNI DIRECTORY" 
           description="The Alumni Directory provides a comprehensive, searchable list of graduates, allowing you to easily reconnect and network." 
-          image="/eventimage.jpg" 
+          image="/explore_event.jpg" 
         />
         <Card 
           title="ALUMNI NETWORKING & MENTORSHIP" 
           description="Join events, reunions, and mentorship programs to expand your professional network and connect with fellow alumni." 
-          image="/eventimage.jpg" 
+          image="/explore_event.jpg" 
         />
         <Card 
           title="ALUMNI NETWORKING & MENTORSHIP" 
           description="Join events, reunions, and mentorship programs to expand your professional network and connect with fellow alumni." 
-          image="/eventimage.jpg" 
+          image="/explore_event.jpg" 
         />
       </div>
     </div>
