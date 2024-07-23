0a512f69a67fd5c81b773462494f5042cb91400a
Aditya Singh <aditya2007singh2004@gmail.com>
2024-07-23 23:09:17 +0530

    gallery commponenets made

---
+++ src/Page/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from '../Page-Components/Gallery/LandingPage.jsx';
+import Albums from '../Page-Components/Gallery/Albums';
+
+const Gallery = () => {
+  return (
+    <div>
+      <LandingPage />
+      <Albums />
+    </div>
+  );
+}
+
+export default Gallery;
