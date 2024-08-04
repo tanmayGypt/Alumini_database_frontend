a05e96a7f3ba4e5eebccf361529f6eed83ffc2fd
SahilTyagii <tyagi28sahil@gmail.com>
2024-08-04 22:24:04 +0530

    alumni directory api integration

--- src/features/alumniDataSlice.js
+++ src/features/alumniDataSlice.js
@@ -1,66 +1,66 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-    alumniData : [
-        { id: nanoid(),
-          name: "Dummy Name",
-          role: "Web Developer",
-          achievement:
-            "achieved an impressive milestone by securing a ₹1.2 Crore package at a top multinational tech company.",
-          year: "Class of 2015",
-        },
-        { id: nanoid(),
-          name: "Jane Smith",
-          role: "Software Engineer",
-          achievement:
-            "achieved an impressive milestone by securing a ₹1.2 Crore package at a top multinational tech company.",
-          year: "Class of 2015",
-        },
-        { id: nanoid(),
-          name: "Alice Johnson",
-          role: "Data Scientist",
-          achievement:
-            "achieved an impressive milestone by securing a ₹1.2 Crore package at a top multinational tech company.",
-          year: "Class of 2015",
-        },
-        { id: nanoid(),
-          name: "Bob Brown",
-          role: "Product Manager",
-          achievement:
-            "achieved an impressive milestone by securing a ₹1.2 Crore package at a top multinational tech company..",
-          year: "Class of 2015",
-        },
-        { id: nanoid(),
-          name: "Chris Green",
-          role: "DevOps Engineer",
-          achievement:
-            "achieved an impressive milestone by securing a ₹1.2 Crore package at a top multinational tech company.",
-          year: "Class of 2015",
-        },
-    ]
+    alumniData : [],
+    status: 'idle',
+    error: null
 }
+
+const JWTtoken = localStorage.getItem('jwtToken');
+console.log('JWT Token:', JWTtoken);
+
+export const fetchAlumni = createAsyncThunk(
+  'alumniDataSlice/fetchAlumni',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/api/alumni', {
+        headers: {
+          'Authorization': `Bearer ${JWTtoken}`
+        }
+      })
+      console.log("Api response: ", response.data)
+      return response.data
+    } catch (error) {
+      console.log("API error: ", error.response ? error.response.data : error.message)
+      return rejectWithValue(error.response ? error.response.data : error.message)
+    }
+  }
+)
 
 const alumniDataSlice = createSlice({
   name :'alumniDataSlice',
   initialState,
   reducers : {
-    addAlumniData : {
-      reducer : (state , action) => {
-        state.alumniData.push(action.payload)
-      },
-      prepare : (name, role, achievement, year) => ({
-        payload : {
-            id : nanoid(),
-            name,
-            role,
-            achievement,
-            year
-        }
-    })
+    // addAlumniData : {
+    //   reducer : (state , action) => {
+    //     state.alumniData.push(action.payload)
+    //   },
+    //   prepare : (name, role, achievement, year) => ({
+    //     payload : {
+    //         id : nanoid(),
+    //         name,
+    //         role,
+    //         achievement,
+    //         year
+    //     }
+    // })
 
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchAlumni.pending, (state) => {
+          state.status = "loading"
+        })
+        .addCase(fetchAlumni.fulfilled, (state, action) => {
+          state.status = "succeeded"
+          state.alumniData = action.payload
+        })
+        .addCase(fetchAlumni.rejected, (state, action) => {
+          state.status = 'failed'
+          state.error = action.payload
+        })
     }
-  }
 })
 
-export const {addAlumniData} = alumniDataSlice.actions
 export default alumniDataSlice.reducer
